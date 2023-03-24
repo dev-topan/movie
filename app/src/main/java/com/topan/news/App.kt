c2bfc1e2705ba6e9d8dcc6c49b927edf40e8d74b
Topan E <topan.extranepa@ocbcnisp.com>
2023-03-25 00:34:27 +0700

    Initial Project

---
+++ app/src/main/java/com/topan/news/App.kt
@@ -0,0 +1,9 @@
+package com.topan.news
+
+import android.app.Application
+
+/**
+ * Created by Topan E on 24/03/23.
+ */
+class App: Application(){
+}
---
+++ domain/src/main/java/com/topan/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.topan.domain
+
+class MyClass {
+}
